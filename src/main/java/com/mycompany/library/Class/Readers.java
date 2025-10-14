2f156d3c74c6fa39587aeabfdd0a7afe62748699
SanmiguelAyleen <ayleen.dessiresanmiguel@gmail.com>
2025-10-13 22:42:39 -0500

    Trabajo 02

---
+++ src/main/java/com/mycompany/library/Class/Readers.java
@@ -0,0 +1,11 @@
+package com.mycompany.library.Class;
+
+public class Readers {
+    private String cedula;
+    private String barrio;
+    private String horaLlegada;
+    
+    public boolean registrarLector(){
+        return true;
+    }
+}
